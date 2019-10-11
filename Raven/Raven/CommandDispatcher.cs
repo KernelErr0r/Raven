3d88a2bc737b246b54f737a4c87dff186c215cd5
KernelError <villainkernelerror@gmail.com>
2019-10-11 18:55:25 +0200

    Move TypeParserPlaceholder
    Signed-off-by: KernelError <villainkernelerror@gmail.com>

--- Raven/Raven/CommandDispatcher.cs
+++ Raven/Raven/CommandDispatcher.cs
@@ -151,19 +151,5 @@ namespace Raven
                 result.Add(Type.Missing);   
             }
         }
-        
-        private struct TypeParserPlaceholder
-        {
-            public TypeParserAttribute Attribute { get; }
-            public Type Type { get; }
-            public object Instance { get; }
-
-            public TypeParserPlaceholder(TypeParserAttribute attribute, Type type, object instance)
-            {
-                Attribute = attribute;
-                Type = type;
-                Instance = instance;
-            }
-        }
     }
 }
